8ed058846538ee4debf1b455c255a6412439fae8
3POSENJOYER <144881934+3POSENJOYER@users.noreply.github.com>
2025-11-14 18:43:25 +0200

    Update MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -3,7 +3,6 @@ public class MainForm : Form
     private TabManager _tabManager;
     private ThemeManager _themeManager;
     private HistoryService _historyService;
-    private MenuStrip _menuStrip;
 
     public MainForm() { }
 }
